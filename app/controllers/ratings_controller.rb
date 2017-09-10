42b38c5c7c8cd09d01c26a23fa88c304c0f47d7b
Sanjana Sarkar <ssark@seas.upenn.edu>
2017-09-10 16:28:40 -0400

    Update ratings_controller.rb

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,12 +1,6 @@
 class RatingsController < ApplicationController
   before_action :set_rating, only: [:show, :edit, :update, :destroy]
-
-  # GET /ratings/1/edit
-  def edit
-  end
-
-  # PATCH/PUT /ratings/1
-  # PATCH/PUT /ratings/1.json
+  
   def update
     @rating = Rating.find(params[:id])
     if @rating.update_attributes(number: params[:score])
@@ -17,13 +11,11 @@ class RatingsController < ApplicationController
   end
 
   private
-  
-  # Use callbacks to share common setup or constraints between actions.
+ 
   def set_rating
     @rating = Rating.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def rating_params
     params.require(:rating).permit(:id, :number, :user_id, :food_truck_id)
   end
